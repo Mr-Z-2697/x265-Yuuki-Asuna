ad189bd3216c5207200c79431def1005fa0bb842
Xinyue Lu <i@7086.in>
2020-06-28 18:00:48 -0400

    3.4: Fix filter and output setparam

--- source/abrEncApp.cpp
+++ source/abrEncApp.cpp
@@ -255,6 +255,16 @@ namespace X265_NS {
             }
         }
         m_param->isAbrLadderEnable = m_parent->m_numEncodes > 1;
+
+        for (auto &&i : m_cliopt.filters)
+        {
+            i->setParam(m_param);
+            if (i->isFail())
+            {
+                return -1;
+            }
+        }
+
         if (m_cliopt.zoneFile)
         {
             if (!m_cliopt.parseZoneFile())
@@ -592,15 +602,6 @@ ret:
 #endif
             memcpy(&m_parent->m_param[m_id], m_param, sizeof(x265_param));
             /* This allows muxers to modify bitstream format */
-            for (auto &&i : m_cliopt.filters)
-            {
-                i->setParam(m_param);
-                if (i->isFail())
-                {
-                    api->param_free(m_param);
-                    exit(1);
-                }
-            }
             m_cliopt.output->setParam(m_param);
             const x265_api* api = m_cliopt.api;
             ReconPlay* reconPlay = NULL;
@@ -1183,6 +1184,7 @@ ret:
         m_id = id;
         for (int view = 0; view < MAX_VIEWS; view++)
             m_input[view] = parentEnc->m_input[view];
+        m_cliopt = &parentEnc->m_cliopt;
     }
 
     void Reader::threadMain()
@@ -1214,6 +1216,18 @@ ret:
                 src->format = m_parentEnc->m_param->format;
                 if (m_input[view]->readPicture(*src))
                 {
+                    for (auto &&i : m_cliopt->filters)
+                    {
+                        i->processFrame(*src);
+                        if (i->isFail())
+                        {
+                            m_threadActive = false;
+                            m_parentEnc->m_inputOver = true;
+                            m_parentEnc->m_parent->m_picWriteCnt[m_id].poke();
+                            break;
+                        }
+                    }
+
                     dest->poc = src->poc;
                     dest->pts = src->pts;
                     dest->userSEI = src->userSEI;
