e5e3e5a93c59b8e844a1fcb053fbb78b3e131da1
DJATOM <djatom@beatrice-raws.org>
2021-01-15 19:06:29 +0200

    Fix possible deadlock on Ctrl+C 100% positives at vpy and avs inputs 30% positives at y4m input

--- source/abrEncApp.cpp
+++ source/abrEncApp.cpp
@@ -977,11 +977,6 @@ ret:
             {
                 general_log(m_param, NULL, X265_LOG_INFO, "aborted at input frame %d, output frame %d in %s\n",
                     m_cliopt.seek + inFrameCount, stats.encodedPictureCount, profileName);
-                if (m_input)
-                {
-                    m_input->release();
-                    m_input = NULL;
-                }
             }
 
             api->param_free(m_param);
@@ -1223,7 +1218,7 @@ ret:
             {
                 x265_picture* dest = (m_parentEnc->m_param->numViews > 1) ? m_parentEnc->m_parent->m_inputPicBuffer[view][writeIdx] : m_parentEnc->m_parent->m_inputPicBuffer[m_id][writeIdx];
                 src->format = m_parentEnc->m_param->format;
-                if (m_input[view]->readPicture(*src))
+                if (m_input[view]->readPicture(*src) && !b_ctrl_c)
                 {
                     for (auto &&i : m_cliopt->filters)
                     {
