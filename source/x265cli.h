70b7b57f4ddcd64df47afac0b690e555eeb5e184
DJATOM <djatom@beatrice-raws.org>
2021-03-17 22:29:26 +0200

    CLI and Input: add --reader-options

--- source/x265cli.h
+++ source/x265cli.h
@@ -97,6 +97,7 @@ static const struct option long_options[] =
     { "seek",           required_argument, NULL, 0 },
     { "frame-skip",     required_argument, NULL, 0 },
     { "frames",         required_argument, NULL, 'f' },
+    { "reader-options", required_argument, NULL, 0 },
     { "recon",          required_argument, NULL, 'r' },
     { "recon-depth",    required_argument, NULL, 0 },
     { "no-wpp",               no_argument, NULL, 0 },
@@ -422,6 +423,7 @@ static const struct option long_options[] =
         int64_t prevUpdateTimeFile;
         char* vf;
         vector<Filter*> filters;
+        const char* readerOpts;
 
         int argCnt;
         char** argString;
@@ -472,6 +474,7 @@ static const struct option long_options[] =
             saveLevel = 0;
             numRefs = 0;
             argCnt = 0;
+            readerOpts = NULL;
         }
 
         void destroy();
