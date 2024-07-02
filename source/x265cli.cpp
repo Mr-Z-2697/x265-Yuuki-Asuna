50c874d64c96c9fb2c64ffa76c7d92913341e519
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2024-07-03 03:58:49 +0800

    pure magic

--- source/x265cli.cpp
+++ source/x265cli.cpp
@@ -312,7 +312,7 @@ namespace X265_NS {
            "                                 Default %d\n", param->rc.aqMode);
         H0("   --[no-]hevc-aq                Mode for HEVC Adaptive Quantization. Default %s\n", OPT(param->rc.hevcAq));
         H0("   --aq-strength <float>         Reduces blocking and blurring in flat and textured areas (0 to 3.0). Default %.2f\n", param->rc.aqStrength);
-        H0("   --aq-bias-strength <float>    Sets dark bias strength in AQ modes that use it. This acts as a factor of the bias_strength which is the same as aq-strength. Default %.2f\n", param->rc.aqBiasStrength);
+        H0("   --aq-bias-strength <float>    Sets dark bias strength in AQ modes that use it. This acts as a factor by which the bias_strength which was the same as aq-strength is multiplied. Default %.2f\n", param->rc.aqBiasStrength);
         H0("   --qp-adaptation-range <float> Delta QP range by QP adaptation based on a psycho-visual model (1.0 to 6.0). Default %.2f\n", param->rc.qpAdaptationRange);
         H0("   --[no-]aq-motion              Block level QP adaptation based on the relative motion between the block and the frame. Default %s\n", OPT(param->bAQMotion));
         H1("   --[no-]sbrc                   Enables the segment based rate control. Default %s\n", OPT(param->bEnableSBRC));
