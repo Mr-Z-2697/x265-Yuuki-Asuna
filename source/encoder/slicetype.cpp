b4f9f3fa4d64577827a5648f74f213fc0eabc8a0
sinsanction <1602723930@qq.com>
2025-03-26 17:02:58 +0800

    Re-add consecutive B-frames histogram that was mistakenly deleted when removing histogram based scene change algorithm

--- source/encoder/slicetype.cpp
+++ source/encoder/slicetype.cpp
@@ -1143,6 +1143,8 @@ Lookahead::Lookahead(x265_param *param, ThreadPool* pool)
             break;
         }
     }
+
+    memset(m_histogram, 0, sizeof(m_histogram));
 }
 
 #if DETAILED_CU_STATS
@@ -2272,6 +2274,7 @@ void Lookahead::slicetypeDecide()
                         list[newbFrames - 1]->m_lowres.bLastMiniGopBFrame = true;
                     list[newbFrames]->m_lowres.leadingBframes = newbFrames;
                     m_lastNonB = &list[newbFrames]->m_lowres;
+                    m_histogram[newbFrames]++;
 
                     /* insert a bref into the sequence */
                     if (m_param->bBPyramid && newbFrames)
@@ -2358,6 +2361,7 @@ void Lookahead::slicetypeDecide()
                         list[newbFrames - 1]->m_lowres.bLastMiniGopBFrame = true;
                 list[newbFrames]->m_lowres.leadingBframes = newbFrames;
                 m_lastNonB = &list[newbFrames]->m_lowres;
+                m_histogram[newbFrames]++;
 
                 /* insert a bref into the sequence */
                 if (m_param->bBPyramid && (newbFrames- listReset) > 1)
@@ -2441,6 +2445,7 @@ void Lookahead::slicetypeDecide()
             list[bframes - 1]->m_lowres.bLastMiniGopBFrame = true;
             list[bframes]->m_lowres.leadingBframes = bframes;
             m_lastNonB = &list[bframes]->m_lowres;
+            m_histogram[bframes]++;
 
             /* insert a bref into the sequence */
             if (m_param->bBPyramid && !brefs)
@@ -2553,6 +2558,7 @@ void Lookahead::slicetypeDecide()
             list[bframes - 1]->m_lowres.bLastMiniGopBFrame = true;
         list[bframes]->m_lowres.leadingBframes = bframes;
         m_lastNonB = &list[bframes]->m_lowres;
+        m_histogram[bframes]++;
 
         /* insert a bref into the sequence */
         if (m_param->bBPyramid && bframes > 1 && !brefs)
