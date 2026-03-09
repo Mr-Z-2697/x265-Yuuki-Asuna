38517648b310dd656396f1566326959773fd4741
Lathambigai <lathambigai.kesava@multicorewareinc.com>
2026-03-09 16:21:44 +0530

    Fix Build Warnings

--- source/x265cli.h
+++ source/x265cli.h
@@ -434,7 +434,7 @@ static const struct option long_options[] =
         char** orgArgv;
         char** argString;
         char *stringPool;
-        char* inputfn[MAX_VIEWS] = {NULL};
+        char* inputfn[MAX_VIEWS];
 
         /* ABR ladder settings */
         bool isAbrLadderConfig;
@@ -455,6 +455,8 @@ static const struct option long_options[] =
                 input[i] = NULL;
             for (int i = 0; i < MAX_LAYERS; i++)
                 recon[i] = NULL;
+            for (int i = 0; i < MAX_VIEWS; i++)
+                inputfn[i] = NULL;
             output = NULL;
             qpfile = NULL;
             zoneFile = NULL;
