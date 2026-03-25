3ac4313ae59a42cb544a2226541b31e280cb4dcd
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2026-03-25 10:23:47 +0800

    Tweak psy-rdoq
    1) to somewhat compensate different effectiveness between rdoq levels.
    2) 1 is too high (IMO), for level 1 at least. maybe I'll adjust level 2 value later.

--- source/common/param.cpp
+++ source/common/param.cpp
@@ -653,7 +653,7 @@ int x265_param_default_preset(x265_param* param, const char* preset, const char*
             param->limitReferences = 3;
             param->bEnableEarlySkip = 0;
             param->rdoqLevel = 1;
-            param->psyRdoq = 1.0;
+            param->psyRdoq = 0.5;
             param->subpelRefine = 3;
             param->searchMethod = X265_STAR_SEARCH;
             param->maxNumReferences = 4;
@@ -893,6 +893,7 @@ int x265_param_default_preset(x265_param* param, const char* preset, const char*
                     [[fallthrough]];
                 case 6:
                     param->rdoqLevel=2;
+                    param->psyRdoq=1.0;
                     param->subpelRefine=7; /* not really very computationally expensive, nor important.
                                             in x264 the subme parameter is actually multiple options
                                             merged together, x265 has individual parameters for them. */
